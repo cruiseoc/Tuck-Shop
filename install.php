8982f18b0142734e9070da34f2c7abee123387ea
cruiseoc <cruise.oc@oundleschool.org.uk>
2022-11-21 12:22:44 +0000

    dfgh

--- install.php
+++ install.php
@@ -12,4 +12,43 @@ Year INT(2) NOT NULL,
 Role TINYINT(1))");
 $stmt->execute();
 $stmt->closeCursor();
+include_once("connection.php");
+$stmt = $conn->prepare("DROP TABLE IF EXISTS TblUser;
+CREATE TABLE TblUsers 
+(UserID INT(4) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+Gender VARCHAR(1) NOT NULL,
+Surname VARCHAR(20) NOT NULL,
+Forename VARCHAR(20) NOT NULL,
+Password VARCHAR(20) NOT NULL,
+House VARCHAR(20) NOT NULL,
+Year INT(2) NOT NULL,
+Role TINYINT(1))");
+$stmt->execute();
+$stmt->closeCursor();
+include_once("connection.php");
+$stmt = $conn->prepare("DROP TABLE IF EXISTS TblUser;
+CREATE TABLE TblUsers 
+(UserID INT(4) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+Gender VARCHAR(1) NOT NULL,
+Surname VARCHAR(20) NOT NULL,
+Forename VARCHAR(20) NOT NULL,
+Password VARCHAR(20) NOT NULL,
+House VARCHAR(20) NOT NULL,
+Year INT(2) NOT NULL,
+Role TINYINT(1))");
+$stmt->execute();
+$stmt->closeCursor();
+include_once("connection.php");
+$stmt = $conn->prepare("DROP TABLE IF EXISTS TblUser;
+CREATE TABLE TblUsers 
+(UserID INT(4) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+Gender VARCHAR(1) NOT NULL,
+Surname VARCHAR(20) NOT NULL,
+Forename VARCHAR(20) NOT NULL,
+Password VARCHAR(20) NOT NULL,
+House VARCHAR(20) NOT NULL,
+Year INT(2) NOT NULL,
+Role TINYINT(1))");
+$stmt->execute();
+$stmt->closeCursor();
 ?>
