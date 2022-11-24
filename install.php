e45523c7590e40c17105cf872587c5705303155c
cruiseoc <cruise.oc@oundleschool.org.uk>
2022-11-24 11:24:44 +0000

    yay

--- install.php
+++ install.php
@@ -3,9 +3,19 @@ include_once("connection.php");
 $stmt = $conn->prepare("DROP TABLE IF EXISTS pupil;
 CREATE TABLE pupil 
 (UserID INT(4) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-PID VARCHAR(1) NOT NULL,
 Name VARCHAR(20) NOT NULL,
-Balance VARCHAR(20) NOT NULL,)");
+Balance VARCHAR(20) NOT NULL)");
+$stmt->execute();
+$stmt->closeCursor();
+
+$stmt = $conn->prepare("DROP TABLE IF EXISTS Order;
+CREATE TABLE Order
+OrderID INT(20) NOT NULL, 
+(UserID INT(4) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+Date VARCHAR(20) NOT NULL
+Processed VARCHAR(1) NOT NULL
+Delivered VARCHAR(1) NOT NULL
+Cancelled VARCHAR(1) NOT NULL)");
 $stmt->execute();
 $stmt->closeCursor();
 ?>
